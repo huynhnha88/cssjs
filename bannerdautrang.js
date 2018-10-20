bfc6444b58cbc62679b301b57906c1a263795f00
huynhnha88 <44262889+huynhnha88@users.noreply.github.com>
2018-10-20 16:06:47 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://faceseo.vn/bannerweb/hoc-seo-chuyen-sau.jpg' width='500px'/><br/>13 Kỹ thuật đẩy top vượt trội</a>. Học tại nhà CEO Faceseo. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='https://1.bp.blogspot.com/-Jo_WWWGtTU8/W8h5P3WZ67I/AAAAAAAAAfY/kWZq1HiNBXAbDZ6IyG9Aw6KWAehgsg3GQCLcBGAs/s1600/abcd.png' width='500px'/><br/>Máy tính chính  hãng</a>. Cybergame. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
